d806f4c2fcee42e4597cf6a36915ec114c9dd8eb
linhcao1611 <mrlinh1611@gmail.com>
2016-06-22 11:58:12 -0700

    check if file not found
    handle different input path

--- Split.py
+++ Split.py
@@ -1,18 +1,30 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader 
-
+import os.path
+import sys
 
 inFilePath = input('Enter pdf file path: ')
-inFile = PdfFileReader(open(inFilePath,'rb'))
+if not os.path.isfile(inFilePath):
+	sys.exit("No file exist!!")
+pdfReader = PdfFileReader(open(inFilePath,'rb'))
 
-print(inFile.getDocumentInfo())
-print(inFile.getPageLayout())
-print(inFile.getPageMode())
-print(inFile.getOutlines())
-print(inFile.getXmpMetadata())
+print(pdfReader.getPageLayout())
+print(pdfReader.getPageMode())
+print(pdfReader.getOutlines())
+print(pdfReader.getXmpMetadata())
 
-for i in range(inFile.getNumPages()):
-    p = inFile.getPage(i)
+dotIndex = inFilePath.find(".")
+beginIndex = inFilePath.find("/")
+if beginIndex == -1:
+	beginIndex = 0
+	fileName = ''
+else:
+	fileName = inFilePath[beginIndex+1:dotIndex]
+print('filename: '+fileName)
+print(inFilePath[:beginIndex])
+
+for i in range(pdfReader.getNumPages()):
+    p = pdfReader.getPage(i)
     outFile = PdfFileWriter()
     outFile.addPage(p)
-    with open('page-%02d.pdf' % i, 'wb') as f:
+    with open(inFilePath[:beginIndex+1]+ fileName+'-%04d.pdf' % (i+1), 'wb') as f:
         outFile.write(f)
